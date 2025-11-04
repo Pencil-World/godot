13c12a6adadfed32f0ab079e13ee023d68d86505
Craner <iayfnp@gmail.com>
2025-11-04 01:35:29 -0500

    done with everything except microscopic changes

--- custom.py
+++ custom.py
@@ -1,51 +1,49 @@
-# # custom.py
-# # command line: scons platform=web target=template_release tools=no debug_symbols=no lto=full optimize=size disable_3d=yes
+# custom.py
+# command line: scons platform=web target=template_release
 
-# # run this every time after opening terminal
-# # emsdk_env.bat
+# run this every time after opening terminal
+# emsdk_env.bat
 
-# # base
-# platform="web"
-# target="template_release"
-# debug_symbols="no"
-# tools="no"
+# base
+debug_symbols="no"
+tools="no"
 
-# # https://docs.godotengine.org/en/4.4/contributing/development/compiling/optimizing_for_size.html
-# lto="full"
-# optimize="size"
-# module_text_server_adv_enabled="yes"
-# module_text_server_fb_enabled="yes"
-# disable_3d="yes"
-# disable_advanced_gui="no"
+# https://docs.godotengine.org/en/4.4/contributing/development/compiling/optimizing_for_size.html
+lto="full"
+optimize="size"
+module_text_server_adv_enabled="yes"
+module_text_server_fb_enabled="yes"
+disable_3d="yes"
+disable_advanced_gui="no"
 
-# # options
-# deprecated="no"
-# minizip="no"
-# vulkan="no"
-# use_volk="no"
+# options
+deprecated="no"
+minizip="no"
+vulkan="no"
+use_volk="no"
 
-# # Generated using https://godot-build-options-generator.github.io
-# module_camera_enabled = "no"
-# module_csg_enabled = "no"
-# module_dds_enabled = "no"
-# module_enet_enabled = "no"
-# module_gltf_enabled = "no"
-# module_gridmap_enabled = "no"
-# module_hdr_enabled = "no"
-# module_jsonrpc_enabled = "no"
-# module_ktx_enabled = "no"
-# module_meshoptimizer_enabled = "no"
-# module_mobile_vr_enabled = "no"
-# module_multiplayer_enabled = "no"
-# module_navigation_enabled = "no"
-# module_noise_enabled = "no"
-# module_openxr_enabled = "no"
-# module_raycast_enabled = "no"
-# module_squish_enabled = "no"
-# module_tga_enabled = "no"
-# module_theora_enabled = "no"
-# module_upnp_enabled = "no"
-# module_vhacd_enabled = "no"
-# module_webrtc_enabled = "no"
-# module_websocket_enabled = "no"
-# module_webxr_enabled = "no"
+# Generated using https://godot-build-options-generator.github.io
+module_camera_enabled = "no"
+module_csg_enabled = "no"
+module_dds_enabled = "no"
+module_enet_enabled = "no"
+module_gltf_enabled = "no"
+module_gridmap_enabled = "no"
+module_hdr_enabled = "no"
+module_jsonrpc_enabled = "no"
+module_ktx_enabled = "no"
+module_meshoptimizer_enabled = "no"
+module_mobile_vr_enabled = "no"
+module_multiplayer_enabled = "no"
+module_navigation_enabled = "no"
+module_noise_enabled = "no"
+module_openxr_enabled = "no"
+module_raycast_enabled = "no"
+module_squish_enabled = "no"
+module_tga_enabled = "no"
+module_theora_enabled = "no"
+module_upnp_enabled = "no"
+module_vhacd_enabled = "no"
+module_webrtc_enabled = "no"
+module_websocket_enabled = "no"
+module_webxr_enabled = "no"
